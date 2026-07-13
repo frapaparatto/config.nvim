b75c6949bc05dd24dcef5a9f6d438819134735ce
Francesco Paparatto <francescopaparatto@gmail.com>
2026-07-13 09:20:58 +0200

    change neovim colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,7 +13,18 @@ return {
   },
 }
 --]]
+-- lua/plugins/rose-pine.lua
+--[[
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
+}
+--]]
 
+--[[
 return {
   {
     "tjdevries/colorbuddy.nvim",
@@ -24,6 +35,7 @@ return {
     end,
   },
 }
+--]]
 
 --return {
 --  "neanias/everforest-nvim",
@@ -38,39 +50,18 @@ return {
 --}
 --
 --
---return {
---  "metalelf0/black-metal-theme-neovim",
---  lazy = false,
---  priority = 1000,
---  config = function()
---    require("black-metal").setup({
---      -- optional configuration here
---      theme = "marduk",
---      -- theme = "thyrfing",
---    })
---    require("black-metal").load()
---  end,
---}
-
---return {
---  "vague-theme/vague.nvim",
---  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
---  priority = 1000, -- make sure to load this before all the other plugins
---  config = function()
---    -- NOTE: you do not need to call setup if you don't want to.
---    require("vague").setup({
---      -- optional configuration here
---    })
---    vim.cmd("colorscheme vague")
---    vim.cmd([[
---        highlight Normal guibg=NONE ctermbg=NONE
---        highlight NormalNC guibg=NONE ctermbg=NONE
---        highlight EndOfBuffer guibg=NONE ctermbg=NONE
---        highlight FloatBorder guibg=NONE ctermbg=NONE
---        highlight NormalFloat guibg=NONE ctermbg=NONE
---          ]])
---  end,
---}
+return {
+  "vague-theme/vague.nvim",
+  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other plugins
+  config = function()
+    -- NOTE: you do not need to call setup if you don't want to.
+    require("vague").setup({
+      -- optional configuration here
+    })
+    vim.cmd("colorscheme vague")
+  end,
+}
 
 --return {
 --	"AlexvZyl/nordic.nvim",
@@ -80,21 +71,6 @@ return {
 --	config = function()
 --		vim.cmd("colorscheme nordic")
 --		--vim.cmd("colorscheme nord")
---		vim.cmd([[
---  highlight Normal guibg=NONE ctermbg=NONE
---  highlight NormalNC guibg=NONE ctermbg=NONE
---  highlight EndOfBuffer guibg=NONE ctermbg=NONE
---  highlight FloatBorder guibg=NONE ctermbg=NONE
---  highlight NormalFloat guibg=NONE ctermbg=NONE
---    ]])
 --	end,
 --}
 --
---return {
---  "webhooked/kanso.nvim",
---  lazy = false,
---  priority = 1000,
---  config = function()
---    vim.cmd("colorscheme kanso")
---  end,
---}
